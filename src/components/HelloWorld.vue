588ebdc8784d1c05374b16990d51ab99d3d942f6
liuyihong.ext <liuyihong.ext@geniusafc.com>
2022-01-25 10:13:04 +0800

    更新02

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,57 @@
 <template>
-  <a-menu
-    id="dddddd"
-    v-model:openKeys="openKeys"
-    v-model:selectedKeys="selectedKeys"
-    style="width: 256px"
-    mode="inline"
-    @click="handleClick"
-  >
-    <a-sub-menu key="sub2" @titleClick="titleClick">
-      <template #icon>
-        <AppstoreOutlined />
-      </template>
-      <template #title>目录 Two</template>
-      <a-menu-item key="5">菜单 5</a-menu-item>
-      <a-menu-item key="6">菜单 6</a-menu-item>
-      <a-sub-menu key="sub3" title="Submenu">
-        <a-menu-item key="7">菜单 7</a-menu-item>
-        <a-menu-item key="8">菜单 8</a-menu-item>
-      </a-sub-menu>
-    </a-sub-menu>
-    <a-sub-menu key="sub4">
-      <template #icon>
-        <SettingOutlined />
-      </template>
-      <template #title>目录 Three</template>
-      <a-menu-item key="9">菜单 9</a-menu-item>
-      <a-menu-item key="10">菜单 10</a-menu-item>
-      <a-menu-item key="11">菜单 11</a-menu-item>
-      <a-menu-item key="12">菜单 12</a-menu-item>
-    </a-sub-menu>
-  </a-menu>
+  <a-layout>
+    <!-- 左侧菜单 -->
+    <a-layout-sider>
+      <a-menu
+        id="dddddd"
+        v-model:openKeys="openKeys"
+        v-model:selectedKeys="selectedKeys"
+        style="width: 100%; background-color: rgb(165, 161, 161)"
+        mode="inline"
+        @click="handleClick"
+      >
+        <child-app></child-app>
+        <a-sub-menu key="sub2" @titleClick="titleClick">
+          <template #icon>
+            <AppstoreOutlined />
+          </template>
+          <template #title>目录 Two</template>
+          <a-menu-item key="5">菜单 5</a-menu-item>
+          <a-menu-item key="6">菜单 6</a-menu-item>
+          <a-sub-menu key="sub3" title="Submenu">
+            <a-menu-item key="7">菜单 7</a-menu-item>
+            <a-menu-item key="8">菜单 8</a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+        <a-sub-menu key="sub4">
+          <template #icon>
+            <SettingOutlined />
+          </template>
+          <template #title>目录 Three</template>
+          <a-menu-item key="9">菜单 9</a-menu-item>
+          <a-menu-item key="10">菜单 10</a-menu-item>
+          <a-menu-item key="11">菜单 11</a-menu-item>
+          <a-menu-item key="12">菜单 12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <!-- 右侧内容 -->
+    <a-layout>
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+    </a-layout>
+  </a-layout>
 </template>
 <script>
 import { defineComponent, ref, watch } from "vue";
 import { AppstoreOutlined, SettingOutlined } from "@ant-design/icons-vue";
+import childApp from "./basic-module/src/components/subMenu.vue";
 export default defineComponent({
   name: "HelloWorld",
   components: {
     AppstoreOutlined,
     SettingOutlined,
+    childApp,
   },
 
   setup() {
@@ -68,3 +81,8 @@ export default defineComponent({
   },
 });
 </script>
+<style scoped type="scss">
+.aa {
+  background-color: rgb(165, 161, 161);
+}
+</style>
